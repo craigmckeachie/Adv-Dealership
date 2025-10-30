41d11d1e621cdef064d719ad6d4293ea04104a45
Craig McKeachie <craigdmckeachie@gmail.com>
2025-10-30 13:29:21 -0400

    Dealership class started

---
+++ src/main/java/com/yearup/dealership/Dealership.java
@@ -0,0 +1,22 @@
+package com.yearup.dealership;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+}
---
+++ src/main/java/com/yearup/dealership/UserInterface.java
@@ -0,0 +1,19 @@
+package com.yearup.dealership;
+
+public class UserInterface {
+
+
+
+    public UserInterface(){
+
+    }
+
+    public void display(){
+
+        System.out.println("Dealership App");
+    }
+
+    private
+
+
+}
